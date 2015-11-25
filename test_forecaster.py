97bca1d8ea9190f702f1daa924e06b31ee738d3d
Uma Nagaswamy <unagaswamy@umas-mbp.attlocal.net>
2015-11-25 17:30:18 -0600

    test_forecaster:refactored using parametrization, test passed

--- test_forecaster.py
+++ test_forecaster.py
@@ -7,12 +7,15 @@ from forecaster import Forecaster, WeatherService
 def mock_ws():
     return Mock(spce=WeatherService)
 
-def test_rain_when_barometer_rising(mock_ws):
+@pytest.mark.parametrize(
+                "reading, expected_forecast",
+                [
+                    ('rising', 'Going to rain'),
+                    ('falling', 'Looks clear'),
+                ]
+)
+def test_forecast(reading, expected_forecast, mock_ws):
     forecaster = Forecaster(mock_ws)
-    mock_ws.barometer.return_value = 'rising'
-    assert forecaster.forecast() == 'Going to rain'
+    mock_ws.barometer.return_value = reading
+    assert forecaster.forecast() == expected_forecast
 
-def test_clear_when_barometer_falling(mock_ws):
-    forecaster = Forecaster(mock_ws)
-    mock_ws.barometer.return_value = 'falling'
-    assert forecaster.forecast() == 'Looks clear'
