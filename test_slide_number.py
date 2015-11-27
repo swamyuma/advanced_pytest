c4b72aa7aede6ecca5438f3d17d6961cc99caf47
swamyuma <swamyuma@users.noreply.github.com>
2015-11-27 14:45:49 -0600

    removed specific and generalized  slide number

--- test_slide_number.py
+++ test_slide_number.py
@@ -1,7 +1,7 @@
 # content of test_sample.py
 def test_answer(slide_number):
-    if slide_number == "111A":
+    if slide_number == "Slide1":
         print "from test_answer {sn}".format(sn=slide_number)
-    elif slide_number == "3066P":
+    elif slide_number == "Slide2":
         print "from test_answer {sn}".format(sn=slide_number)
     #assert 0 # to see what was printed
