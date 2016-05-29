d3fd9c7d704cea37d2fb3d8388c54e1ac73d7c2e
swamyuma <swamyuma@users.noreply.github.com>
2016-05-29 16:40:36 -0500

    added comments for fixture

--- test_website_title.py
+++ test_website_title.py
@@ -6,6 +6,11 @@ browsers = {
                 'chrome': webdriver.Chrome('chromedriver 2'),
           }
 
+# dependency consrtuction is moved out of test case 
+# use scope=session to use the same instance of a browser to test both sites
+# pytest will cache this fixture for the entire test session
+# pass in params which contains the browser names and details
+# use addfinalizer for a clean teardown after the tests are completed
 @pytest.fixture(scope='session', params=browsers.keys())
 def driver(request):
     browser = browsers[request.param]
